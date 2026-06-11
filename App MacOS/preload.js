fff87e1629b73dd3d320b4582a1428f2316ef4c9
IceCream <maeldupuy49@gmail.com>
2026-06-11 23:51:53 +0200

    ajustement

--- App MacOS/preload.js
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('electronApp', {
-  platform:           process.platform,
-  isDesktop:          true,
-  completeOnboarding: (serverUrl) => ipcRenderer.send('onboarding-complete', serverUrl),
-});
